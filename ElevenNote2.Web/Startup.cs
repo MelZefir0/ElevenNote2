97fdf14245e250478603bfbea7217eea19f677aa
MelZefir0 <wanderlust3x3@gmail.com>
2017-11-14 14:40:28 -0500

    initial commit

---
+++ ElevenNote2.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElevenNote2.Web.Startup))]
+namespace ElevenNote2.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
