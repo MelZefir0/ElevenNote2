09bf943cef301d9399566ae00f5f80a1bd68466a
MelZefir0 <wanderlust3x3@gmail.com>
2017-11-15 10:04:04 -0500

    NoteEntity

---
+++ ElevenNote.Data/NoteEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    [Table("Note")]
+    public class NoteEntity
+    {
+        [Key]
+        public int NoteId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [MaxLength(500)]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime CreatedUtc { get; set; }
+
+        //question mark after data type allows nulls. makes it optional. DateTime needs some kind of direction. C# will require a MinValue unless
+        //otherwise specified.
+        public DateTime? ModifiedUtc { get; set; }
+    }
+}
